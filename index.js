29cf08d5ffca7d6a1f41532751ba24590b11a08c
Jake Jarvis <jake@jarv.is>
2020-06-16 11:52:33 -0400

    add very basic linting & expand CI tests to Node >=10

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Netlify Plugin: netlify-plugin-cache
+// https://github.com/jakejarvis/netlify-plugin-cache
+//
+// This plugin is essentially a wrapper around Netlify's native `cache-utils`:
 // https://github.com/netlify/build/blob/master/packages/cache-utils/README.md
 
 module.exports = {
-  // try to restore cache before build begins, if it exists
+  // Try to restore cache before build begins, if it exists
   onPreBuild: async ({ utils: { cache }, inputs }) => {
     if (await cache.restore(inputs.paths)) {
       const files = await cache.list(inputs.paths)
@@ -11,24 +15,23 @@ module.exports = {
     }
   },
 
-  // only save/update cache if build was successful
+  // Only save/update cache if build was successful
   onSuccess: async ({ utils: { cache, status }, inputs }) => {
     if (await cache.save(inputs.paths)) {
       const files = await cache.list(inputs.paths)
       console.log(`Successfully cached: ${inputs.paths.join(', ')} ... ${files.length} files in total.`)
 
-      // show success & more detail in deploy summary
+      // Show success & more detail in deploy summary
       status.show({
         title: `${files.length} files cached`,
         summary: 'These will be restored on the next build! ⚡',
-        text: `${inputs.paths.join(', ')}`,
+        text: `${inputs.paths.join(', ')}`
       })
     } else {
-      // this probably happened because the default `paths` is set, so provide instructions to fix
+      // This probably happened because the default `paths` is set, so provide instructions to fix
       console.log(`Attempted to cache: ${inputs.paths.join(', ')} ... but failed. :(`)
-      console.log(`Try setting the 'paths' input appropriately in your netlify.toml configuration.`)
-      console.log(`More details: https://jrvs.io/netlify-cache-usage`)
+      console.log('Try setting the \'paths\' input appropriately in your netlify.toml configuration.')
+      console.log('More details: https://jrvs.io/netlify-cache-usage')
     }
-  },
+  }
 }
-  
